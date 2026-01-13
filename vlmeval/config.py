92c97f798e4f2648fb9f69e31280329f925b11fe
chandrabhuma <119724110+chandrabhuma@users.noreply.github.com>
2026-01-13 17:22:35 +0530

    Update config.py

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -1969,7 +1969,7 @@ model_groups = [
     long_vita_series, ristretto_series, kimi_series, aguvis_series, hawkvl_series,
     flash_vl, kimi_vllm_series, oryx_series, treevgr_series, varco_vision_series, qtunevl_series, 
     xvl_series, thyme_series, logics_series, cosmos_series, keye_series, qianfanvl_series, 
-    lfm2vl_series, interns1_series, insight_v_series
+    lfm2vl_series, interns1_series, insight_v_series, ayavision_series
 ]
 
 for grp in model_groups:
